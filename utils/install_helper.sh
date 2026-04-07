51ac21d5f66df3867175f9372ea4c92f4a70669c
Alex Fishman <alex@fuse-t.org>
2026-04-07 18:36:06 +0200

    fuse-t changes
    Signed-off-by: Alex Fishman <alex@fuse-t.org>

--- utils/install_helper.sh
+++ utils/install_helper.sh
@@ -12,8 +12,9 @@ prefix="${MESON_INSTALL_DESTDIR_PREFIX}"
 
 mkdir -p "${prefix}/${sbindir}"
 
-ln -svf --relative "${prefix}/${bindir}/sshfs" \
-   "${prefix}/${sbindir}/mount.sshfs"
+sshfs_target="$(python3 -c 'import os, sys; print(os.path.relpath(sys.argv[1], sys.argv[2]))' \
+   "${prefix}/${bindir}/sshfs" "${prefix}/${sbindir}")"
 
-ln -svf --relative "${prefix}/${bindir}/sshfs" \
-   "${prefix}/${sbindir}/mount.fuse.sshfs"
+ln -svf "${sshfs_target}" "${prefix}/${sbindir}/mount.sshfs"
+
+ln -svf "${sshfs_target}" "${prefix}/${sbindir}/mount.fuse.sshfs"
